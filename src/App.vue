427a469d7f5f97c12300c45fd41f021983e4cabe
murilo9 <murilohenriquematias@gmail.com>
2021-10-16 16:59:05 -0300

    added fonts and more styling; removed unecessary stuff

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,46 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-app-bar app elevation="0" class="mx-3"> Top Bar </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container>CONTAINER</v-container>
     </v-main>
+    <v-footer>Footer</v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 });
 </script>
+
+<style>
+@font-face {
+  font-family: "Fira Sans";
+  src: url("./assets/font/FiraSans/FiraSans-Regular.ttf") format("truetype");
+}
+@font-face {
+  font-family: "Fira Sans";
+  src: url("./assets/font/FiraSans/FiraSans-Bold.ttf") format("truetype");
+  font-weight: bold;
+}
+@font-face {
+  font-family: "Fira Sans";
+  src: url("./assets/font/FiraSans/FiraSans-Medium.ttf") format("truetype");
+  font-weight: 600;
+}
+@font-face {
+  font-family: "Fira Sans";
+  src: url("./assets/font/FiraSans/FiraSans-Light.ttf") format("truetype");
+  font-weight: 200;
+}
+</style>
