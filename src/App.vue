877abac1648ecf49229cc155eddab3ca516e92ac
murilo9 <murilohenriquematias@gmail.com>
2021-10-16 17:36:49 -0300

    correctly applied vuetify styling overriding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar app elevation="0" class="mx-3"> Top Bar </v-app-bar>
+    <v-app-bar app elevation="0" class="mx-3" prominent>
+      <div>TODAY</div>
+      <div>WED</div>
+      <div>OCT 4</div>
+    </v-app-bar>
 
     <v-main>
       <v-container>CONTAINER</v-container>
@@ -23,7 +27,7 @@ export default Vue.extend({
 });
 </script>
 
-<style>
+<style lang="scss">
 @font-face {
   font-family: "Fira Sans";
   src: url("./assets/font/FiraSans/FiraSans-Regular.ttf") format("truetype");
@@ -42,5 +46,26 @@ export default Vue.extend({
   font-family: "Fira Sans";
   src: url("./assets/font/FiraSans/FiraSans-Light.ttf") format("truetype");
   font-weight: 200;
+}
+/* Applies font-family */
+.v-application {
+  font-family: "Fira Sans", "Roboto", sans-serif !important;
+}
+
+/* Applies flex col to app bar */
+.v-toolbar__content {
+  flex-direction: column;
+  align-items: center !important;
+}
+
+/* Applies background and font colors */
+.theme--light.v-application {
+  color: $fontColor !important;
+}
+
+/* Applies top bar transparency*/
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background-color: transparent !important;
+  border-bottom: 1px solid $borderColor;
 }
 </style>
