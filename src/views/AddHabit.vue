d2d50890c09b2cba4608fb98fa210354107feab4
murilo9 <murilohenriquematias@gmail.com>
2021-10-16 19:50:08 -0300

    pre-implemented add habit page

---
+++ src/views/AddHabit.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main> Add Habit </v-main>
+    <v-footer>Footer</v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "AddHabit",
+};
+</script>
+
+<style>
+</style>
