f68956ddf463fb7fbb2905951dbf6521c0cc6cc8
murilo9 <murilohenriquematias@gmail.com>
2021-10-16 22:31:58 -0300

    displaying current date on top bar

--- src/components/MobileTopBar.vue
+++ src/components/MobileTopBar.vue
@@ -12,15 +12,29 @@
     >
       <div class="text-h5 mb-3 mb-sm-0">TODAY</div>
       <div>
-        <div class="body-1 text-center">WED</div>
-        <div class="body-1">OCT 4</div>
+        <div class="body-1 text-center">{{ weekLabel }}</div>
+        <div class="body-1">{{ monthLabel }}</div>
       </div>
     </v-container>
   </v-app-bar>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  computed: {
+    weekLabel() {
+      return new Date().toUTCString().split(",")[0].toUpperCase();
+    },
+    monthLabel() {
+      return (
+        new Date().toUTCString().split(" ")[2].toUpperCase() +
+        " " +
+        new Date().getDate()
+      );
+    },
+  },
+});
 </script>
 
 <style>
