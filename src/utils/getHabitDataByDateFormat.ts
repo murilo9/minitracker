9bb3e763070fdd300ac935cea9ed7c4c58ba35ba
murilo9 <murilohenriquematias@gmail.com>
2021-10-17 20:24:16 -0300

    fixed getHabitDataByData functions names

--- src/utils/getHabitDataByDateFormat.ts
+++ src/utils/getHabitDataByDateFormat.ts
@@ -1,14 +1,14 @@
+import DateFormat from "@/types/DateFormat";
 import Habit from "@/types/Habit";
 import HabitData from "@/types/HabitData";
 import compareDateFormats from "./compareDateFormats";
-import getDateFormat from "./getDateFormat";
 
 /**
  * Returns the habit status for the specified date.
  * @param dateToFind 
  * @param habit 
  */
-export default function getHabitDataByDate(dateToFind: Date, habit: Habit): HabitData | null {
-  const habitData = habit.data.find(data => compareDateFormats(getDateFormat(dateToFind), data.date))
+export default function getHabitDataByDateFormat(dateToFind: DateFormat, habit: Habit): HabitData | null {
+  const habitData = habit.data.find(data => compareDateFormats(dateToFind, data.date))
   return habitData || null;
 }
