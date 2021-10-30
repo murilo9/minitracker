f3f86d21a0d6e1750c3c45638c4765a1aa03261e
murilo9 <murilohenriquematias@gmail.com>
2021-10-30 14:32:30 -0300

    Update

---
+++ precache-manifest.7f8b29114acb237949b89f295cdb840d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48465a863cc394762724",
+    "url": "/minitracker/css/app.ede67176.css"
+  },
+  {
+    "revision": "ad239eeaac03c5245003",
+    "url": "/minitracker/css/chunk-vendors.29105063.css"
+  },
+  {
+    "revision": "a1acb00f5bffd4e7b86f0cfb89fa184a",
+    "url": "/minitracker/fonts/FiraSans-Bold.a1acb00f.ttf"
+  },
+  {
+    "revision": "034229170eceddca9e300e8d2a4c8208",
+    "url": "/minitracker/fonts/FiraSans-Light.03422917.ttf"
+  },
+  {
+    "revision": "d7fafde2fe8dd421fc8db1a7f48965d6",
+    "url": "/minitracker/fonts/FiraSans-Medium.d7fafde2.ttf"
+  },
+  {
+    "revision": "895f5b025a6cc4924b263f6beb06c777",
+    "url": "/minitracker/fonts/FiraSans-Regular.895f5b02.ttf"
+  },
+  {
+    "revision": "07113bd99f55ec81ff3fa1ee217297d7",
+    "url": "/minitracker/index.html"
+  },
+  {
+    "revision": "48465a863cc394762724",
+    "url": "/minitracker/js/app.e6039b49.js"
+  },
+  {
+    "revision": "ad239eeaac03c5245003",
+    "url": "/minitracker/js/chunk-vendors.73579621.js"
+  },
+  {
+    "revision": "f1d2cf6d04bc4f6ad4a88fa26f4e98f1",
+    "url": "/minitracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/minitracker/robots.txt"
+  }
+]);
